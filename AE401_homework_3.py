d66c0c8a365a8ca2919d730fe0374a081624e43e
Henrick97 <93032277+Henrick97@users.noreply.github.com>
2021-11-13 19:00:26 +0800

    Update AE401_homework_3.py

--- AE401_homework_3.py
+++ AE401_homework_3.py
@@ -1,17 +1,15 @@
 import random
 num = random.randint(1,20)
 user = input()
-user = int()
+user = int(user)
 x = 0
-while user != num:
+while x <= 5:
     x = x+1
     if x == 5:
         print('輸了')
-    user = input()
-    user = int(user)
     if user < num:
         print("太小")
     elif user > num:
         print('太大')
     elif user == num:
-        print('玩了x次')
+        print('玩了',x,'次')
